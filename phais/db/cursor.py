7d90791f870c81807a54bb1bc951a626788b4061
James Payor <j.payor@hotmail.com>
2013-10-25 02:35:50 +1100

    Minor fix to the cursor's __len__

--- phais/db/cursor.py
+++ phais/db/cursor.py
@@ -3,4 +3,4 @@ from MySQLdb.cursors import DictCursor
 # A small wrapper of a DictCursor that provides support for len(cursor) and cursor.rows().
 # Far more impressive functionality is expected.
 class Cursor(DictCursor):
-	__len__ = rows = lambda self: self.rowcount
+	__len__ = rows = lambda self: max(self.rowcount, 0)
