05ff53bb9dfb858a237dfd11ab1229153ab8a357
James Payor <j.payor@hotmail.com>
2013-10-26 02:37:23 +1100

    DBObject refactored.

--- phais/db/dbobject.py
+++ phais/db/dbobject.py
@@ -6,18 +6,15 @@ from . import db
 
 class DBObject(object):
 	
-	def __init__(self, table, properties, **kwargs):
-		self._dbo_table = table
-		self._dbo_properties = dict((name, data) for name, data in properties.items() if name != 'id')
-		if kwargs:
-			self._dbo_load(**kwargs)
+	def __init__(self):
+		pass
 
 
-	def _dbo_load(self, **kwargs):
+	def load(self, **kwargs):
 
 		with db as c:
 
-			c.execute('SELECT ' + ', '.join(self._dbo_properties.keys()) + ' FROM {} WHERE '.format(self._dbo_table) + ' AND '.join(key + '=%s' for key in kwargs.keys()) + ' LIMIT 1', kwargs.values())
+			c.execute('SELECT ' + ', '.join(self.dbproperties.keys()) + ' FROM {} WHERE '.format(self.dbtable) + ' AND '.join(key + '=%s' for key in kwargs.keys()) + ' LIMIT 1', kwargs.values())
 
 			if not c: return False
 
@@ -32,13 +29,10 @@ class DBObject(object):
 			setattr(self, key, value)
 
 
-	def _dbo_save(self):
+	def save(self):
 
 		with db as c:
 
-			data = dict((prop, getattr(self,prop)) for prop in self._dbo_properties.keys())
+			data = dict((prop, getattr(self,prop)) for prop in self.dbproperties.keys())
 
-			c.execute('UPDATE {} SET '.format(self._dbo_table) + ', '.join(key + '=%s' for key in data.keys()), data.values())
-
-	save = _dbo_save
-	load = _dbo_load
+			c.execute('UPDATE {} SET '.format(self.dbtable) + ', '.join(key + '=%s' for key in data.keys()), data.values())
