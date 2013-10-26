509a2b1da02358e5758f495c18f8251ffbde6818
James Payor <j.payor@hotmail.com>
2013-10-26 15:32:17 +1100

    favicon.ico moved.

--- phais/redirect.py
+++ phais/redirect.py
@@ -1,7 +1,8 @@
 from phais import app
 from flask import redirect
 
+# Temporary, should give this location in the webpages:
 @app.route('/favicon.ico')
 def favicon_redirect():
-	return redirect('/static/favicon.ico')
+	return redirect('/static/img/favicon.ico')
 
