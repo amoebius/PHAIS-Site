06e3ac81d3cf708a32906bf9f517ad22e7604272
James Payor <j.payor@hotmail.com>
2013-10-26 20:12:45 +1100

    404 error page updated slightly (no longer used for debugging).

--- phais/errors/error_404.py
+++ phais/errors/error_404.py
@@ -5,7 +5,4 @@ from phais.db import db
 
 @app.errorhandler(404)
 def not_found(error):
-	with db as c:
-		c.execute('SELECT * FROM user')
-		return str(c.fetchall())
 	return render_template('error_404.html'), 404
