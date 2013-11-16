6e14bd05214cc9d7dcbc93c5e85b3ac826043837
James Payor <j.payor@hotmail.com>
2013-11-16 11:29:15 +1100

    New SSE test envisaged.

--- phais/handlers/sse.py
+++ phais/handlers/sse.py
@@ -1,14 +1,41 @@
 from phais import app
-from flask import Response, render_template
+from flask import Response, render_template, redirect, request
+from random import randint
 
-@app.route('/sse_source')
+
+from collections import deque
+from time import sleep
+users = dict()
+
+class Responder(object):
+	def __init__(self, user):
+		self.user = user
+		self.q = list()
+	def generate(self, n=0):
+		while True:
+			while len(self.q) <= n:
+				sleep(0.5)
+			while len(self.q) > n:
+				yield 'id: %d\ndata: <dt>%s</dt> <dd>%s</dd>\n\n' % (n, self.q[n][0], self.q[n][1])
+				n += 1
+	def addmsg(self, who, msg):
+		self.q.push_back((who,msg))
+
+@app.route('/sse/<user>')
 def sse_request():
-	def generator():
-		for c in 'abcdefg':
-			yield 'data: %s\n\n' % c
+	r = Responder(user)
+	users.append(r)
+	return Response(r.generate(), mimetype='text/event-stream')
 
-	return Response(generator(), mimetype='text/event-stream')
+@app.route('/', methods = ['GET', 'POST'])
+def make_user():
+	if request.method == 'GET':
+		return redirect('/guest%d' % randint(1,1000000))
+	else:
+		for user in users:
+			user.addmsg(request.form.get('from',''), request.form.get('message',''))
+		return ''
 
-@app.route('/')
-def sse_page():
-	return render_template('sse_test.html')
+@app.route('/<user>')
+def sse_page(user):
+	return render_template('sse_test.html', user=user)
