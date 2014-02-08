c48d319cc5b156947dcafa9c06259d31ecf40d00
James Payor <j.payor@hotmail.com>
2014-02-09 05:05:35 +1100

    Database host URL added to configuration, configuration file moved.

--- phais/__init__.py
+++ phais/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask('phais', instance_relative_config = True)
 
-app.config.from_pyfile('phais.cfg')
+app.config.from_pyfile('config.py')
 
 import handlers
--- phais/db/__init__.py
+++ phais/db/__init__.py
@@ -3,7 +3,7 @@ from .cursor import Cursor
 from phais import app
 
 dbconfig = app.config['DB']
-dbc = connect(user = dbconfig.user, passwd = dbconfig.passwd, db = dbconfig.db, cursorclass = Cursor)
+dbc = connect(host = dbconfig.host, user = dbconfig.user, passwd = dbconfig.passwd, db = dbconfig.db, cursorclass = Cursor)
 
 from .dbobject import DBObject
 from .user import User
